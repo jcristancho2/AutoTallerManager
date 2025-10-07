a85d60618b4f083c914e9b1e1cfe2cd230912ae1
Eduardo Castellanos <eduardoeliascastellanospicon@gmail.com>
2025-10-07 11:49:35 -0500

    feat: :sparkles: add UnitOfWOrk & IUnitOfWork

---
+++ AutoTallerManager.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTallerManager.Infrastructure.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public Task<int> SaveChangesAsync(CancellationToken ct = default)
+        => _context.SaveChangesAsync(ct);
+    public async Task ExecuteInTransactionAsync(Func<CancellationToken, Task> operation, CancellationToken ct = default)
+    {
+        await using var tx = await _context.Database.BeginTransactionAsync(ct);
+        try
+        {
+            await operation(ct);
+            await _context.SaveChangesAsync(ct);
+            await tx.CommitAsync(ct);
+        }
+        catch
+        {
+            await tx.RollbackAsync(ct);
+            throw;
+        }
+    }
+
+
+}
