8a4a6302d2499f55744f7c618d4d4e691839e9e6
jcristancho2 <jocristanchool@gmail.com>
2025-10-09 18:21:16 -0500

    feat: :sparkles: creacion de servicio de interfaz de cliente

---
+++ AutoTallerManager.Application/Abstractions/Interfaces/IClienteService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoTallerManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace AutoTallerManager.Application.Abstractions.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<Cliente?> GetByIdAsync(int id, CancellationToken ct = default, params string[] includeProperties);
+        Task<Cliente?> GetByEmailAsync(string email, CancellationToken ct = default);
+        Task<IEnumerable<Cliente>> GetAllAsync(
+            Expression<Func<Cliente, bool>>? filter = null,
+            Func<IQueryable<Cliente>, IOrderedQueryable<Cliente>>? orderBy = null,
+            string includeProperties = "",
+            int? skip = null,
+            int? take = null,
+            CancellationToken ct = default);
+        Task<int> CountAsync(Expression<Func<Cliente, bool>>? filter = null, CancellationToken ct = default);
+        Task<bool> ExistsAsync(Expression<Func<Cliente, bool>> filter, CancellationToken ct = default);
+        Task AddAsync(Cliente cliente, CancellationToken ct = default);
+        void Update(Cliente cliente);
+        void Delete(Cliente cliente);
+    }
+}
