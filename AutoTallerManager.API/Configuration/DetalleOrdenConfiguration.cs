8cbff836326784eca75e61d8ebe5091d65d96419
Eduardo Castellanos <eduardoeliascastellanospicon@gmail.com>
2025-10-09 10:31:01 -0500

    feat: :sparkles: Finish configurations part1

--- AutoTallerManager.API/Configuration/DetalleOrdenConfiguration.cs
+++ AutoTallerManager.API/Configuration/DetalleOrdenConfiguration.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoTallerManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AutoTallerManager.API.Configuration
 {
