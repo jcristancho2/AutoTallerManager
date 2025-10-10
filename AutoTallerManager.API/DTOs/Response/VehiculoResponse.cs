a59301dd9d98572a77485a40f54187b9570218da
LeidyJohanaVillegas <lvillegas5@gmail.com>
2025-10-09 21:52:15 -0500

    feat: DTO Vehicle response added

---
+++ AutoTallerManager.API/DTOs/Response/VehiculoResponse.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AutoTallerManager.API.DTOs.Response
+{
+    public record VehiculoResponse
+    {
+        public int VehiculoId { get; init; }
+        public string? Placa { get; init; }
+        public int Anio { get; init; }
+        public string? VIN { get; init; }
+        public int Kilometraje { get; init; }
+
+        public int ClienteId { get; init; }
+        public int TipoVehiculoId { get; init; }
+        public int MarcaVehiculoId { get; init; }
+        public int ModeloVehiculoId { get; init; }
+    }
+}
