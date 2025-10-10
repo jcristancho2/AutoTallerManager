d7f4885fce7ffbeb47a9beb090688e7c9360dbde
LeidyJohanaVillegas <lvillegas5@gmail.com>
2025-10-09 22:02:33 -0500

    feat: DTO invoice response added

---
+++ AutoTallerManager.API/DTOs/Response/FacturaResponse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AutoTallerManager.API.DTOs.Response
+{
+    public record FacturaResponse
+    {
+        public int FacturaId { get; init; }
+        public DateTime Fecha { get; init; }
+        public decimal Total { get; init; }
+        public int OrdenServicioId { get; init; }
+        public int ClienteId { get; init; }
+        public int PagoId { get; init; }
+    }
+}
