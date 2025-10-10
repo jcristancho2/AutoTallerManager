4ee5b6864c7e1dbd25a355bd518f79bad6d8e214
LeidyJohanaVillegas <lvillegas5@gmail.com>
2025-10-09 22:04:16 -0500

    feat: DTO paginated  response added

---
+++ AutoTallerManager.API/DTOs/Response/PaginatedResponse.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace AutoTallerManager.API.DTOs.Response
+{
+    public record PaginatedResponse<T>
+    {
+        public IReadOnlyCollection<T> Items { get; init; } = Array.Empty<T>();
+        public int TotalCount { get; init; }
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling((double)TotalCount / PageSize);
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
