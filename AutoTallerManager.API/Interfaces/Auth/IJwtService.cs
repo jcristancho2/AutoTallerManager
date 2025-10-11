f74a8890352da1df8dd8d30273479b4724efc662
LeidyJohanaVillegas <lvillegas5@gmail.com>
2025-10-11 03:07:35 -0500

    feat(api): implement JwtService and IJwtService for JWT/refresh token generation

--- AutoTallerManager.API/Interfaces/Auth/IJwtService.cs
+++ AutoTallerManager.API/Interfaces/Auth/IJwtService.cs
@@ -1,9 +1,13 @@
-using System;
+using System.IdentityModel.Tokens.Jwt;
+using AutoTallerManager.Domain.Entities.Auth;
 
 namespace AutoTallerManager.API.Services.Interfaces.Auth;
 
 public interface IJwtService
 {
+    JwtSecurityToken CreateJwtToken(UserMember user);
+    string WriteToken(JwtSecurityToken token);
+    RefreshToken CreateRefreshToken();
 }
 
 
