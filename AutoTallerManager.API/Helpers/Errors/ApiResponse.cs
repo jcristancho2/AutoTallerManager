37a4aac89acf89f6fb9c19b73b4d73afcd7b4915
jcristancho2 <jocristanchool@gmail.com>
2025-10-11 08:29:39 -0500

    refactor: :bug: modificacion de using archivos de error en helper

--- AutoTallerManager.API/Helpers/Errors/ApiResponse.cs
+++ AutoTallerManager.API/Helpers/Errors/ApiResponse.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AutoTallerManager.Shared.API.Errors;
+namespace AutoTallerManager.API.Helpers.Errors;
 
 public class ApiResponse
 {
--- AutoTallerManager.API/Helpers/Errors/ApiValidation.cs
+++ AutoTallerManager.API/Helpers/Errors/ApiValidation.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoTallerManager.Shared.API.Errors;
+using AutoTallerManager.API.Helpers.Errors;
 
 namespace AutoTallerManager.API.Helpers.Errors;
 
